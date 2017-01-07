7f3ce949a4847479eca4ee71cecac3d49b4a049d
Steven Murphy <steve231@gmail.com>
2017-01-07 23:20:50 +0000

    Commit 2

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,7 +1,32 @@
-package PACKAGE_NAME;
-
 /**
  * Created by Steve on 07/01/2017.
+ *
+ * Implement fizz buzz
  */
 public class FizzBuzz {
+
+    private static final int maxCount = 100;
+
+    public static void main(String[] args ) {
+        System.out.println("test");
+
+        int counter = 0;
+
+        while(counter < maxCount) {
+            counter += 1;
+
+            System.out.println(Integer.toString(counter));
+
+            if (counter % 3 == 0 && counter %5 == 0 ) {
+                System.out.println("fizzbuzz");
+            }
+            else if (counter % 3 == 0) {
+                System.out.println("fizz");
+            }
+            else if(counter % 5 == 0) {
+                System.out.println("buzz");
+            }
+        }
+
+    }
 }
